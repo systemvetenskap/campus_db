9a35968b4b26b2eeeca76c6a67a6cba63bcc0d00
eriobe <erik.oberg@miun.se>
2016-05-17 11:10:25 +0200

    Kopplar upp mot databasen samt gör två mindre sqlfrågor

---
+++ expedition/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace expedition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public BindingList<person> personlista = new BindingList<person>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;User Id=webuser;Password=test123;Database=expedition");
+                
+                string sql = "select * from person";
+                conn.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+               // NpgsqlDataReader dr = cmd.ExecuteReader();
+                person p;
+                while (dr.Read())
+                {
+                    p = new person()
+                    {
+                        förnamn = dr["förnamn"].ToString(),
+                        efternamn = dr["efternamn"].ToString(),
+                        id = (int)dr["id"]
+                    };
+                    personlista.Add(p);
+                   // MessageBox.Show(dr["förnamn"].ToString());
+                    listBox1.DisplayMember = "FullständigtNamn";
+                    listBox1.DataSource = personlista;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //finally
+            //{
+            //    conn.Close();
+            //}
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;User Id=webuser;Password=test123;Database=expedition");
+            string lösenord = textBox1.Text;
+            string sql = "select * from person where passwd = :pass";
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("pass", lösenord);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                MessageBox.Show(dr["förnamn"].ToString());
+            }
+            conn.Close();
+
+        }
+    }
+}
---
+++ expedition/person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expedition
+{
+    public class person
+    {
+        public int id { get; set; }
+        public string förnamn { get; set; }
+        public string efternamn { get; set; }
+
+        public string FullständigtNamn
+        {
+            get
+            {
+                return förnamn + " " + efternamn;
+            }
+        }
+    }
+}
